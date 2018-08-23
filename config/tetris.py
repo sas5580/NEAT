1aadaf67c0535f73dca35805bbaa1604cf653542
Saswata Gupta <gupta.saswata@gmail.com>
2018-08-22 20:43:10 -0400

    Figured o0ut and created configuration files

---
+++ config/tetris.py
@@ -0,0 +1,16 @@
+# Game
+HORIZONTAL_BLOCKS = 10
+VERTICAL_BLOCKS = 21
+DROP_SPEED = 1 # second
+SPAWN_POS = (3, 0)
+GRAVITY = 1000 # milliseconds between each move down
+DEFAULT_DELAY = 500
+MAX_DELAY = 2200 # milliseconds
+DELAY_LEEWAY = 300 # milliseconds
+
+
+# Display
+BLOCK_SIZE = 30 # px
+LINES_SENT_SIZE = (50, 50)
+NEXT_BOX_SIZE = (100, 300)
+BORDER_DEPTH = 20 
