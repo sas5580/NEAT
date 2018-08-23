84f8588615a128ccf72c396cc25d0828d4eb5a41
Saswata Gupta <gupta.saswata@gmail.com>
2018-08-22 20:44:29 -0400

    Add main script to init game

---
+++ main.py
@@ -0,0 +1,12 @@
+from view.view import GameView
+
+if __name__ == '__main__':
+    app = GameView()
+    app.run()
+
+
+# DEBUG
+# from copy import deepcopy
+# from game.tetris import Tetris
+# g = Tetris()
+# g.start()
