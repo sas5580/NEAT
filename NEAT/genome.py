c8fd57be4e142d73745910b7dcbfd2ea1282ee43
Saswata Gupta <gupta.saswata@gmail.com>
2018-08-25 21:01:04 -0400

    Added all mutations to Genome

--- NEAT/genome.py
+++ NEAT/genome.py
@@ -1,6 +1,7 @@
 from random import sample, choice, uniform
 
-from NEAT.config import DISABLE_PROB_IF_PARENT_DISABLED
+from NEAT.config import DISABLE_PROB_IF_PARENT_DISABLED, EXCESS_WEIGHT, DISJOINT_WEIGHT, WEIGHT_DIFFERENCE_WEIGHT, \
+    ADD_CONNECTION_MUTATION_ATTEMPTS, ADD_NODE_MUTATION_ATTEMPTS
 from NEAT.node_gene import NodeGene
 from NEAT.connection_gene import ConnectionGene
 
@@ -33,34 +34,116 @@ class Genome:
         pos_connections = inp*hidden + inp*out + hidden*out + (hidden - 1)*(hidden)//2
         return len(self.connections) == pos_connections
 
-    def connectionMutation(self):
+    def addConnectionMutation(self):
         if self._isFullyConnected():
             print('Genome fully connected')
             return
 
         # Find 2 nodes that can have a valid connection and aren't already connected
+        attempts = 1
         node_set = self.nodes.union({self.bias_node})
         node1, node2 = NodeGene.orientNodes(sample(node_set, 1)[0], sample(node_set, 1)[0])
-        while node1.type == node2.type or ConnectionGene.getInnovation(node1, node2) in self.connections:
+        while node1.type == node2.type or node1.depth == node2.depth or ConnectionGene.getInnovation(node1, node2) in self.connections:
+            attempts += 1
+            if attempts > ADD_CONNECTION_MUTATION_ATTEMPTS:
+                print('addConncetionMutation too many attempts')
+                return
             node1, node2 = NodeGene.orientNodes(sample(node_set, 1)[0], sample(node_set, 1)[0])
 
         self._addConnection(ConnectionGene(node1, node2))
     
-    def nodeMutation(self):
+    def deleteConnectionMutation(self):
         if not self.connections:
-            self.connectionMutation()
-
+            return
+        
         connection = choice(list(self.connections.values()))
-        while connection.in_ == self.bias_node:
+        del self.connections[connection.innovation]
+
+        if connection.in_ != NodeGene.Type.HIDDEN and connection.out != NodeGene.Type.HIDDEN:
+            return
+
+        found_in = found_out = False
+        for conn in self.connections.values():
+            if connection.in_ == conn.in_ or connection.in_ == conn.out:
+                found_in = True
+            if connection.out == conn.in_ or connection.out == conn.out:
+                found_out = True
+        
+        if connection.in_ == NodeGene.Type.HIDDEN and not found_in:
+            self.nodes.remove(connection.in_)
+
+        if connection.out == NodeGene.Type.HIDDEN and not found_out:
+            self.nodes.remove(connection.out)
+
+    def addNodeMutation(self):
+        attempt = 1
+        connection = choice(list(self.connections.values()))
+        while not connection.enabled and connection.in_ == self.bias_node:
+            attempt += 1
+            if attempt > ADD_NODE_MUTATION_ATTEMPTS:
+                print('addNodeMutation too many attempts')
+                return
             connection = choice(list(self.connections.values()))
 
-        connection.enabled = False
+        connection.disable()
 
-        new_node = NodeGene(NodeGene.Type.HIDDEN)
+        if connection.in_.depth + 1 == connection.out.depth:
+            # Adding new depth, increment larger depths to make space
+            for node in self.nodes:
+                if node.depth >= connection.out.depth:
+                    node.depth += 1
+
+        new_node = NodeGene(NodeGene.Type.HIDDEN, connection.in_.depth + 1)
         self._addNode(new_node)
 
         self._addConnection(ConnectionGene(connection.in_, new_node, 1.0))
         self._addConnection(ConnectionGene(new_node, connection.out, connection.weight))
+    
+    def deleteNodeMutation(self):
+        first_non_io_node_ind = 0
+        nodes = list(self.nodes)
+        for node in nodes:
+            if node.type == NodeGene.Type.HIDDEN:
+                break
+            first_non_io_node_ind += 1
+
+        if first_non_io_node_ind >= len(nodes) - 1:
+            return
+
+        node = choice(nodes[first_non_io_node_ind:])
+        assert(node.type_ == NodeGene.Type.HIDDEN)
+
+        self.nodes.remove(node)
+
+        self.connections = {
+            innov: conn for innov, conn in self.connections.items() if
+            conn.in_ != node or conn.out != node
+        }
+
+    def toggleEnableMutation(self):
+        connection = choice(self.connections.values())
+        if not connection.enabled:
+            connection.enable()
+        else:
+            # make sure another connection exists from in node
+            found = False
+            for gene in self.connections.values():
+                if gene.in_ == connection.in_:
+                    found = True
+                    break
+            if found:
+                connection.disable()
+
+    def renenableMutation(self):
+        for connection in self.connections.values():
+            if not connection.enabled:
+                connection.enable()
+                break
+    
+    def weightsMutation(self):
+        for connection in self.connections.values():
+            if not connection.frozen:
+                connection.mutateWeight()
 
     # Assunes genome1 is fitter than genome 2
     @classmethod
