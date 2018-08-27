ce16962ce69f6550aabdcf5a66678479237d7099
Saswata Gupta <gupta.saswata@gmail.com>
2018-08-27 14:17:12 -0400

    Started concrete implementations of population and species methods

--- NEAT/population.py
+++ NEAT/population.py
@@ -1,12 +1,13 @@
 from operator import attrgetter
 
+from NEAT.config import STAGNANT_SPECIES_AGE_DIFF
 from NEAT.network import Network
 from NEAT.species import Organism, Species
 
 
 class Population:
     def __init__(self, seed_genomes):
-        self.generation = 0
+        self.generation = 1
         self.max_fitness = 0
         self.organisms = []
         for i, genome in enumerate(seed_genomes):
@@ -17,6 +18,9 @@ class Population:
 
         self.species = []
         self.speciate(self.organisms)
+
+    def calculate_fitness(self):
+        pass
 
     def speciate(self, organisms):
         for org in organisms:
@@ -31,11 +35,44 @@ class Population:
                 specie = Species(org)
                 self.species.append(specie)
 
+
     def next_generation(self):
+        self.calculate_fitness()
+
+        # Adjust fitness based on species size and stagnance
+        # Kill off bottom of species (param for amount)
+        # Calculate average fitnesses of each species
+        # Kill stagnant species
+        # Kill crappy species
+        # give children to species based on how much they contribute to the entire sum of average fitnesses
+        # If not enough children, create more from best species
+        # Speciate based on old organsisms
+        # delete old organisms from population and species
+        population_size = len(self.organisms)
+        average_fitness_sum = 0.0
+
+
+        for sp in self.species:
+            sp.new_gen()
+            sp.compute_adjusted_fitness()
+            sp.sort_and_cull()
+            average_fitness_sum += sp.average_adjusted_fitness
+
+        sp_index = len(self.species)
+        ave_fit_sum_copy = average_fitness_sum
+        while sp_index > 0:
+            sp_index -= 1
+            sp = self.species[sp_index]
+            if sp.age - sp.last_improved_age >= STAGNANT_SPECIES_AGE_DIFF:
+                average_fitness_sum -= sp.average_adjusted_fitness
+                del self.species[sp_index]
+            elif sp.average_adjusted_fitness/ave_fit_sum_copy*population_size < 1:
+                average_fitness_sum -= sp.average_adjusted_fitness
+                del self.species[sp_index]
+
+        children = []
+        for sp in self.species:
+
+            num_children = 0
+
         self.generation += 1
-
-        #for sp in self.species: sp.compute_max_fitness()
-        #self.species.sort(key=attrgetter('max_fitness'), reverse=True)
-
-
-
--- NEAT/species.py
+++ NEAT/species.py
@@ -2,7 +2,7 @@ from operator import attrgetter
 from collections import namedtuple
 
 from NEAT.genome import Genome
-from NEAT.config import COMPTABILITY_THRESHOLD, SPECIES_PENALIZE_AGE, SPECIES_STAGNANT_PENALTY, SURVIVAL_THRESHOLD
+from NEAT.config import COMPTABILITY_THRESHOLD, SPECIES_PENALIZE_AGE, STAGNANT_SPECIES_PENALTY, SURVIVAL_THRESHOLD
 
 Organism = namedtuple('Organism', ['id', 'generation', 'genome', 'network', 'fitness', 'adjusted_fitness', 'eliminate', 'champion'])
 Organism.__new__.__defaults__ = (None, None, False, False)
@@ -13,7 +13,8 @@ class Species:
         self.organisms = [rep_org]
         self.max_fitness = None
         self.max_fitness_lifetime = None
-        self.age = 1
+        self.average_adjusted_fitness = None
+        self.age = 0
         self.last_improved_age = 0
 
     def add(self, org):
@@ -25,30 +26,36 @@ class Species:
     def compatible(self, organism):
         return Genome.compatibility(self.organisms[0].genome, organism.genome) < COMPTABILITY_THRESHOLD
 
+    def new_gen(self):
+        self.age += 1
+
     def compuate_adjusted_fitness(self):
-        age_debt = (self.age - self.last_improved_age + 1 - SPECIES_PENALIZE_AGE)
         size = len(self.organisms)
 
         for org in self.organisms:
             org.adjusted_fitness = org.fitness
-
-            if age_debt >= 0:
-                org.adjusted_fitness *= SPECIES_STAGNANT_PENALTY
 
             if org.adjusted_fitness < 0:
                 org.adjusted_fitness = 0.0001
 
             org.adjusted_fitness /= size
 
+            self.average_adjusted_fitness += org.adjusted_fitness
 
-    def sort_and_label(self):
-        self.organisms.sort(key=attrgetter('adjusted_fitness'), reverse=True)
-        best_fitness = self.organisms[0].adjusted_fitness
+        self.average_adjusted_fitness /= len(self.organisms)
+
+    def sort_and_cull(self):
+        self.organisms.sort(key=attrgetter('fitness'), reverse=True)
+        best_fitness = self.organisms[0].fitness
         if best_fitness > self.max_fitness_lifetime:
             self.last_improved_age = self.age
             self.max_fitness_lifetime = best_fitness
 
+        new_av_fitness = self.average_adjusted_fitness*len(self.organisms)
         num_parents = int(SURVIVAL_THRESHOLD*(len(self.organisms) + 1))
 
-        for org in self.organisms[num_parents:]:
-            org.eliminate = True
+        for i in range(num_parents, len(self.organisms)):
+            new_av_fitness -= self.organisms[i].adjusted_fitness
+            del self.organisms[i]
+
+        self.average_adjusted_fitness = new_av_fitness/len(self.organisms)
