04006f3f72536aa46d213faa81f3555a52dde81f
Saswata Gupta <gupta.saswata@gmail.com>
2019-01-23 21:19:26 -0500

    Added genomes and play to allow palying with genomes

--- apps/snake/play.py
+++ apps/snake/play.py
@@ -1,7 +1,19 @@
+import sys
+import pickle
+
 from apps.view_lib.view import GameView
 from apps.snake.view import SnakeView
 from apps.snake.game import Snake
+from apps.snake.train import play_snake_with_view
+
 
 if __name__ == '__main__':
-    app = GameView(controller=SnakeView(Snake(speed_multiplier=1.0)))
-    app.run()
+    if len(sys.argv) > 1:
+        genome_path = sys.argv[1]
+        with open(genome_path, 'rb') as f:
+            org = pickle.load(f)
+        play_snake_with_view(org)
+
+    else:
+        app = GameView(controller=SnakeView(Snake(speed_multiplier=1.0)))
+        app.run()
--- apps/snake/train.py
+++ apps/snake/train.py
@@ -47,8 +47,6 @@ def make_move(game, network, state=None):
         angle = -math.atan2(head[1] - food[1], head[0] - food[0])
     elif game.direction == Action.RIGHT:
         angle = -math.atan2(food[1] - head[1], food[0] - head[0])
-    else:
-        assert False
 
     inp.append(angle / math.pi)
     scores = list(network.activate(inp))
@@ -58,25 +56,33 @@ def make_move(game, network, state=None):
     ]
     game.action(Output.get_dir(action, game.direction))
 
+    # checking for stale game
+    if state is None:
+        state = {'no_score_change': 0}
+
+    if state.get('last_score') == game.score:
+        state['no_score_change'] += 1
+    else:
+        state['no_score_change'] = 0
+
+    if state['no_score_change'] >= 1000:
+        game.game_over = True
+
+    state['last_score'] = game.score
+
 def play_snake(network):
     game = Snake(speed_multiplier=1e9)
     game.MOVE_RATE = 0
     game.start()
 
-    no_score_change = 0
-    while not game.game_over and no_score_change < 400 and game.steps < 10000:
-        score = game.score
+    state = {'no_score_change': 0}
 
-        make_move(game, network)
+    while not game.game_over and game.steps < 10000:
+        make_move(game, network, state)
         game.step()
 
-        if score != game.score:
-            no_score_change = 0
-        else:
-            no_score_change += 1
-
     fitness = calculate_fitness(game)
-    if no_score_change >= 400:
+    if state['no_score_change'] >= 400:
         fitness -= 3.99
 
     if game.steps >= 10000:
@@ -88,12 +94,14 @@ def snake_fitness(network):
     scores = (play_snake(network) for _ in range(10))
     return sum(scores) / 5
 
-def play_snake_with_view(network):
+def play_snake_with_view(organism):
+    network = Network(organism.genome.nodes, organism.genome.bias_node, organism.genome.connections)
+
     game = Snake(speed_multiplier=3.0)
 
     move_fn = partial(make_move, game, network)
 
-    view = GameView(controller=SnakeView(game), ai_controller=move_fn, ai_state=1)
+    view = GameView(controller=SnakeView(game), ai_controller=move_fn, ai_state={'no_score_change': 1})
     view.run()
 
 if __name__ == '__main__':
@@ -102,4 +110,4 @@ if __name__ == '__main__':
     with open(f'apps/snake/genomes/{datetime.date.today()}_{org.fitness:.4f}.pickle', 'wb') as f:
         pickle.dump(org, f)
 
-    play_snake_with_view(Network(org.genome.nodes, org.genome.bias_node, org.genome.connections))
+    play_snake_with_view(org)
